c07a39e9837d7af8c5879a0c0ccedbb50c12136b
Bruno <bvaleiro@gmail.com>
2024-08-29 20:42:45 -0300

    fix:add generated static params

--- src/app/portifolio/[id]/page.tsx
+++ src/app/portifolio/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { Sidebar } from "@/components/Sidebar";
-
+export function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }]
+}
 export default function Page({ params }: { params: { slug: string } }) {
   return (
     <>
